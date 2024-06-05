eebf175beaf5a2c2a91ed9159510d1d929df701e
jack <jack@itsjack.cloud>
2024-06-05 21:40:42 +0100

    remove avm and use own

--- main.tf
+++ main.tf
@@ -9,15 +9,64 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.location
 }
 
-module "avm-res-compute-virtualmachine" {
-  source  = "Azure/avm-res-compute-virtualmachine/azurerm"
-  version = "0.14.0"
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = module.naming.virtual_network.name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+}
 
-  location                = var.location
-  name                    = module.naming.virtual_machine.name
-  resource_group_name     = azurerm_resource_group.resource_group.name
-  virtualmachine_sku_size = "Standard_B2s"
-  zone                    = 1
+resource "azurerm_subnet" "subnet" {
+  name                 = join("-", [module.naming.subnet.name, "one"])
+  resource_group_name  = azurerm_resource_group.resource_group.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = module.naming.public_ip.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "network_interface" {
+  name                = module.naming.network_interface.name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+    private_ip_address_allocation = "Dynamic"
+
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "linux_virtual_machine" {
+  name                            = module.naming.virtual_machine.name
+  resource_group_name             = azurerm_resource_group.resource_group.name
+  location                        = azurerm_resource_group.resource_group.location
+  size                            = "Standard_F2"
+  admin_username                  = "adminuser"
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 
   custom_data = base64encode(
     <<-EOF
--- variables.tf
+++ variables.tf
@@ -9,3 +9,8 @@ variable "name" {
   description = "Used in naming of resources."
   default     = "powerpipe"
 }
+
+variable "admin_password" {
+  type        = string
+  description = "The password for the virtual machine."  
+}
