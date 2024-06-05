af2e905c0acad1e019356c2d1b4f393c0b37182c
jack <jack@itsjack.cloud>
2024-06-05 19:34:08 +0100

    add backend for demo

--- backend.tf
+++ backend.tf
@@ -1,5 +1,9 @@
 terraform {
   backend "azurerm" {
+    resource_group_name   = "rg-storage-sandbox-uks"
+    storage_account_name  = "sacpcterraformbackend"
+    container_name        = "tfstate"
+    key                   = "powerpipe_demo.tfstate"
   }
 }
 
