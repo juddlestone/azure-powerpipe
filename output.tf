5b20de16b88e7689cb96baeb500e05bb619f635f
jack <jack@itsjack.cloud>
2024-06-05 22:16:35 +0100

    also remove output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
-output "powerpipe_ip_address" {
-  value = "http://${azurerm_public_ip.public_ip.ip_address}:9093"
-}
+# output "powerpipe_ip_address" {
+#   value = "http://${azurerm_public_ip.public_ip.ip_address}:9093"
+# }
