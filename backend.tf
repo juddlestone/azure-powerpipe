3437e4d67a5a135bbaa8164843846ff8f92afb12
jack <jack@itsjack.cloud>
2024-06-05 21:13:15 +0100

    change container name

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-storage-sandbox-uks"
     storage_account_name = "sacpcterraformbackend"
-    container_name       = "tfstate"
+    container_name       = "terraform"
     key                  = "powerpipe_demo.tfstate"
     use_oidc             = true
   }
